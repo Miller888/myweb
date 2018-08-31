0e7c7d7ef14eaec1d7772133a2b32bef34f8629d
Harry Huang <theforce.forwork@gmail.com>
2018-08-31 20:55:24 +0800

    java8 Date Object

---
+++ Student.Harry/src/main/java/Student/Harry/Date/test.java
@@ -0,0 +1,20 @@
+package Student.Harry.Date;
+
+import java.time.LocalDate;
+import java.time.chrono.MinguoDate;
+import java.time.chrono.JapaneseDate;
+import java.time.chrono.ThaiBuddhistDate;
+import java.util.Date;
+
+public class test {
+//New function from Java 8
+	public static void main(String[] args) {
+		MinguoDate mingo=MinguoDate.now();
+		System.out.println(mingo);
+		JapaneseDate jp=JapaneseDate.now();
+		System.out.println(jp);
+		ThaiBuddhistDate thai=ThaiBuddhistDate.now();
+		System.out.println(thai);
+	}
+
+}
