6cf7bbb94c8e2c87e09c6537bc9f16444d68cd5e
Harry Huang <theforce.forwork@gmail.com>
2018-08-31 20:59:51 +0800

    Java8 Date

--- Student.Harry/src/main/java/Student/Harry/Date/test.java
+++ Student.Harry/src/main/java/Student/Harry/Date/test.java
@@ -7,14 +7,15 @@ import java.time.chrono.ThaiBuddhistDate;
 import java.util.Date;
 
 public class test {
-//New function from Java 8
+	// New function from Java 8
 	public static void main(String[] args) {
-		MinguoDate mingo=MinguoDate.now();
+		MinguoDate mingo = MinguoDate.now();
 		System.out.println(mingo);
-		JapaneseDate jp=JapaneseDate.now();
+		JapaneseDate jp = JapaneseDate.now();
 		System.out.println(jp);
-		ThaiBuddhistDate thai=ThaiBuddhistDate.now();
+		ThaiBuddhistDate thai = ThaiBuddhistDate.now();
 		System.out.println(thai);
+
 	}
 
 }
